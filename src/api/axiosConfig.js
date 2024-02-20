91ef6a2e2cac56c23092c233f2e451c636d67749
shivam <shivamvikhar5500@gmail.com>
2024-02-20 12:47:00 +0530

    update api key

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const tmdbAxios = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: "5abed1d77ecac13dcfddc58836b0c088",
+    api_key: "ce7528240b37762fcf8f05ddd0dba1ab",
     language: "en-US",
   },
 });
